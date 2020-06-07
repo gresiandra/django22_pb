2511fe96f51da49966ce2bf47266d24314b89d97
gresiandra <gresiandra@gmail.com>
2020-06-07 14:37:03 +0700

    media

--- django_pb/settings.py
+++ django_pb/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/' 
+MEDIA_URL = '/media/'  
