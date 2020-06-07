40a61a4d7a3c442b4bbf7de254934ffb39cdac2e
gresiandra <gresiandra@gmail.com>
2020-06-07 14:20:09 +0700

    media

--- jobs/models.py
+++ jobs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class job(models.Model):
     
-    image = models.ImageField(upload_to='')
+    image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
 
