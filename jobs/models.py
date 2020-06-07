e5c28f45c018be9e0bef962de8764d198caf3492
gresiandra <gresiandra@gmail.com>
2020-06-07 14:06:29 +0700

    etc etc

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class job(models.Model):
+    
+    image = models.ImageField(upload_to='')
+    summary = models.CharField(max_length=200)
+
+
+def __str__(self):
+    return "{}. {}".format(self.id, self.summary)
