19805cbaf4cf58634a2469867b1a960a5ab4d81e
gresiandra <gresiandra@gmail.com>
2020-06-08 09:56:00 +0700

    homepage

--- django_pb/urls.py
+++ django_pb/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', include(('jobs.urls','jobs'), namespace='jobs')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
