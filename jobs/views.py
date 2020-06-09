a9964d49d41415ba4d3b04459469398bc1c1c637
gresiandra <gresiandra@gmail.com>
2020-06-09 08:49:29 +0700

    Shows Past Jobs/Achivements

--- jobs/views.py
+++ jobs/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import job
 
 # Create your views here.
 
 def index(request):
+
+    jobs = job.objects.all()
     
     context = {
-
+        'jobs':jobs,
     }
 
     return render(request, 'jobs/index.html', context)
